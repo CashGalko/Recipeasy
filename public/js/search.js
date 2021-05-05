c408747cde5a6e7a44bece8f8a7af2d3a463a2d5
Kevin Eddie <keddie024@gmail.com>
2021-05-05 17:50:27 -0400

    search route working, but handlebars must be implemented correctly to display on page

--- public/js/search.js
+++ public/js/search.js
@@ -1,19 +1,24 @@
-const { Recipe } = require('.../models');
-
 const searchDB = async (event) => {
     event.preventDefault();
+    console.log("clicked");
 
-    const query = document.querySelector('#search').value.trim().toLowerCase();
-    const recipeData = await Recipe.findAll();
+    const query = document.querySelector('#search').value.trim();
 
-
-
-
+    if (query) {
+        const response = await fetch('/api/recipe/' + query, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response);
+        if (response.ok) {
+            
+            alert('Success!');
+        } else {
+            alert('Failed to post.');
+        }
+    }
 }
 
-
-
-
 document
-  .querySelector('.search-form')
-  .addEventListener('submit', searchDB);
+    .querySelector('.search-form')
+    .addEventListener('submit', searchDB);
