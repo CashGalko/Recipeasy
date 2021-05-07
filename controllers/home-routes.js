aad226f726e3c2f407e0242f5b09fb690a453dae
keddie024 <keddie024@gmail.com>
2021-05-07 15:14:08 -0400

    updated home-route to be in sync with the main branch

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { Recipe } = require('../models');
+const { Op } = require("sequelize");
 
 router.get('/', async (req, res) => {
   try {
     if (req.session.loggedIn) {
-      res.render('mealPlanner', { loggedIn: req.session.loggedIn });
+      res.render('mealPlanner');
     } else {
       let random = Math.floor(Math.random() * 1086);
       const recipeData = await Recipe.findByPk(random);
@@ -17,13 +18,54 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/password', async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    } else {
+      res.render('password');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-router.get('/search', (req, res) => {
-  if (!req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  } else {
-    res.render('search',  { loggedIn: req.session.loggedIn });
+router.get('/search', async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    } else {
+      res.render('search');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/search/:query?', async (req, res) => {
+  console.log("Should be seeing search page");
+  if(req.params.query) {
+    try {
+      console.log("Querying for: " + req.params.query);
+      const recipeData = await Recipe.findAll({
+        where: {
+          title: {
+            [Op.substring]: `${req.params.query}`,
+          }
+        }
+      });
+      const fixedRecipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+      // console.log(fixedRecipes);
+      res.render('search', {fixedRecipes});
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
 });
 
@@ -34,7 +76,7 @@ router.get('/new', async (req, res) => {
       res.redirect('/');
       return;
     } else {
-      res.render('userRecipes',  { loggedIn: req.session.loggedIn });
+      res.render('userRecipes');
     }
   } catch (err) {
     console.log(err);
@@ -48,7 +90,21 @@ router.get('/saved', async (req, res) => {
       res.redirect('/');
       return;
     } else {
-      res.render('savedRecipes',  { loggedIn: req.session.loggedIn });
+      res.render('savedRecipes');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/pdf', async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    } else {
+      res.render('pdf');
     }
   } catch (err) {
     console.log(err);
