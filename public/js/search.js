9673d1fe61992053adcec3800d2db391e213442a
CGalko <cash.galko12@ncf.edu>
2021-05-08 17:18:55 -0400

    Got recipes to display as accordian cards for saved & searched recipes. Need to figure out how to get the recipe's ID from the searched object so that it can be saved properly. The logout script is currently broken and needs to be fixed.

--- public/js/search.js
+++ public/js/search.js
@@ -6,6 +6,38 @@ const searchDB = async (event) => {
     window.location.href = '/search/' + query;
 }
 
+const saveCurrentRecipe = async (event) => {
+    event.preventDefault();
+    const recipeID = this.id
+    console.log(recipeID);
+        // Saves a recipe from the search results to the user's SavedRecipes table
+    const saveRecipe = await fetch('/api/saved/new', {
+      method: 'POST',
+      body: JSON.stringify({ recipeID }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
 document
     .querySelector('.search-form')
     .addEventListener('submit', searchDB);
+document
+    .querySelector('.save-button')
+    .addEventListener('click', saveCurrentRecipe);
