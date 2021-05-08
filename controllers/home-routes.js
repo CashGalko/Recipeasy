37c47fa16b52ecc9006fc43b24bbf5c528a04b0b
CGalko <cash.galko12@ncf.edu>
2021-05-08 15:46:33 -0400

    Removed unnecessary . from handlebar script links to fix numerous issues we were having linking the client side js. Cleaned up the code by migrating client side get requests into backend routes for both the saved and mealplanner home routes. Still need to display the data correctly.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,28 @@
 const router = require('express').Router();
-const { Recipe } = require('../models');
+const { Recipe, SavedRecipe, User } = require('../models');
 const { Op } = require("sequelize");
 
 router.get('/', async (req, res) => {
   try {
     if (req.session.loggedIn) {
-      res.render('mealPlanner', { loggedIn: req.session.loggedIn });
+      if(req.session.userID) {
+        
+          console.log("Searching for all recipes for User: " + req.session.userID);
+          const savedData = await User.findOne({
+            include: [{ model: Recipe }],
+            where: {
+              id: req.session.userID,
+            }
+      
+          });
+          // Might need to change variables if we are getting weird scope errors.
+          const savedRecipeData = savedData.recipes;
+          const fixedRecipeData = savedRecipeData.map((recipe) => recipe.get({ plain: true }));
+          console.log(fixedRecipeData);
+          res.render('mealPlanner', { loggedIn: req.session.loggedIn, fixedRecipeData });
+      
+      }
+      
     } else {
       let random = Math.floor(Math.random() * 1086);
       const recipeData = await Recipe.findByPk(random);
@@ -84,27 +101,35 @@ router.get('/new', async (req, res) => {
   }
 });
 
+// Renders all of the data needed for the saved recipe page
 router.get('/saved', async (req, res) => {
   try {
     if (!req.session.loggedIn) {
       res.redirect('/');
       return;
     } else {
-      res.render('savedRecipes', { loggedIn: req.session.loggedIn });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get('/pdf', async (req, res) => {
-  try {
-    if (!req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    } else {
-      res.render('pdf');
+      if(req.session.userID) {
+        try {
+          console.log("Searching for all recipes for User: " + req.session.userID);
+          const savedData = await User.findOne({
+            include: [{ model: Recipe }],
+            where: {
+              id: req.session.userID,
+            }
+      
+          });
+          
+          const savedRecipeData = savedData.recipes;
+          const fixedRecipeData = savedRecipeData.map((recipe) => recipe.get({ plain: true }));
+          console.log(fixedRecipeData);
+          res.render('savedRecipes', { loggedIn: req.session.loggedIn, fixedRecipeData });
+          
+        } catch (err) {
+          console.log(err);
+          res.status(500).json(err);
+        }
+      }
+      
     }
   } catch (err) {
     console.log(err);
