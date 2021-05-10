38b4ae4cfac328a5e3d242b6cce6bbce5f233ce1
CGalko <cash.galko12@ncf.edu>
2021-05-09 20:42:06 -0400

    Hidden deployment url test

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('mysql://zcl1ei7olpowi9ca:yhkjpefj1axtbj7g@wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ceyduc901jpjt2x3');
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 
 module.exports = sequelize;
